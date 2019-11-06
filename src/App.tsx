90942cd3bd9415d655f78030f74c4ee59400745b
Brian Forbis <bforbis@athenahealth.com>
2019-11-06 15:11:52 -0500

    Use state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-const App: React.FC = () => {
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+function App(): JSX.Element {
+  const [users, setUsers] = useState<User[]>([
+    { id: 1, name: 'Cory', email: 'a@h.com' },
+    { id: 2, name: 'Megan', email: 'b@h.com' },
+    { id: 3, name: 'Tami', email: 'c@h.com' }
+  ]);
+
+  const h1Style: React.CSSProperties = {
+    color: 'red',
+    marginBottom: 20
+  };
+
+  function handleDelete(id: number): void {
+    const newUsers = users.filter(user => user.id !== id);
+    setUsers(newUsers);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="header" style={h1Style}>
+        Users
+      </h1>
+      <ul>
+        {users.map(user => (
+          <li>
+            <button onClick={() => handleDelete(user.id)}>Delete</button>
+            {user.name}
+          </li>
+        ))}
+      </ul>
+      <label htmlFor="firstName">FirstName</label>
+      <input id="firstName" type="text"></input>
+      <p>My app</p>
+    </>
   );
 }
 
