9e925cabd49c6dc182a9cf3ec905d9139d111318
Brian Forbis <bforbis@athenahealth.com>
2019-11-07 14:53:50 -0500

    Add edit button

--- src/Users.tsx
+++ src/Users.tsx
@@ -40,6 +40,14 @@ function Users(): JSX.Element {
             <tr key={user.id}>
               <td>
                 <button onClick={() => handleDelete(user.id)}>Delete</button>
+                <Link to={`/user/${user.id}`}>
+                  <button>
+                    Edit{' '}
+                    <span aria-label="athena pencil" role="img">
+                      ✏️
+                    </span>
+                  </button>
+                </Link>
               </td>
               <td>{user.id}</td>
               <td>{user.name}</td>
--- src/api/users.ts
+++ src/api/users.ts
@@ -14,7 +14,7 @@ export async function getUsers(): Promise<User[]> {
 
 export async function deleteUser(id: number): Promise<void> {
   const response = await fetch(`/api/users/${id}`, {
-    method: 'delete'
+    method: 'DELETE'
   });
   if (response.ok) {
     return response.json();
@@ -24,7 +24,21 @@ export async function deleteUser(id: number): Promise<void> {
 
 export async function addUser(user: Omit<User, 'id'>): Promise<User> {
   const response = await fetch(`/api/users`, {
-    method: 'post',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  });
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Bad network response.');
+}
+
+export async function editUser(user: User): Promise<User> {
+  const response = await fetch(`/api/users/${user.id}`, {
+    method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
     },
